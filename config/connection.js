1ad37ec06506a73d04140a10ee9aa8574235b273
Mariana Tapia <mglopez2113@gmail.com>
2022-05-23 20:25:57 -0700

    added node modules to gitignore file and updtd index.js in routes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const {connect, connection} = require('mongoose');
 
-connect('mongodb://localhost/socialnetworkDB',{
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/socialNetworkDB";
+
+connect(connectionString,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
